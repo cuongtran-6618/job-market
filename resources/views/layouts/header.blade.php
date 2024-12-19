657a08a51c780151144d453f5c942b4c456c2f65
Cuong Tran <mailtocuong@gmail.com>
2024-12-19 23:14:10 +0200

    (style) cleanup menu

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,20 +1,3 @@
-<header class="container flex-center">
-    <div class="inline-block">
-        <a href="/" title="homepage">Job market</a>
-        <div class="hamburger" onclick="toggleMobileMenu()">
-            <div class="hamburger-line"></div>
-            <div class="hamburger-line"></div>
-            <div class="hamburger-line"></div>
-        </div>
-    </div>
-    <nav class="float-right inline-block">
-        <div>Services</div>
-        <x-menu />
-    </nav>
-    <div class="links float-right inline-block">
-        <ul class="flex-center">
-            <li class="small-margin"><a href="/user/register" title="register a new user">Login/Register</a></li>
-            <li class="small-margin">Become a Professional</li>
-        </ul>
-    </div>
+<header class="container">
+    <x-menu />
 </header>
