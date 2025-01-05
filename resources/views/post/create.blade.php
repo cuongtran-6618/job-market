98eabe03b6fac820de66249f230ca333f011c0ec
Cuong Tran <mailtocuong@gmail.com>
2025-01-05 22:04:39 +0200

    feat: style for creat post form

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,34 +1,37 @@
 @extends("layouts/layout")
 @section("content")
-<strong><label>Create a new post for {{ $selectedCategory }}</label></strong>
-<form action="{{ route('post.create') }} " method="post">
-    @csrf
-    @method('post')
-    <div class="">
-        <label>Description</label>
-        <textarea name="name" placeholder="job description" alt="description"></textarea>
+<?php
+    $inputStyle="mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500";
+    $labelStyle="block mb-2 text-sm font-medium text-gray-900 dark:text-white";
+?>
+<div class="container mx-auto main-content">
+    <div class="flex flex-col items-center">
+        <form action="{{ route('post.create') }}" class="min-w-80 mx-auto" method="post">
+            <div class="text-4xl text-left capitalize mb-4 md:mb-12"><h1>{{ $selectedCategory }}</h1></div>
+            <div class="flex flex-col items-start gap-4">
+                @csrf
+                @method('post')
+                <div class="w-full">
+                    <label class="{{ $labelStyle }}">City</label>
+                    <select name="city" class="{{ $inputStyle }}">
+                        @foreach ($cities as $id => $name)
+                            <option value="{{$id}}">{{ $name }}</option>
+                        @endforeach
+                    </select>
+                    <label class="{{ $labelStyle }}">Description</label>
+                    <textarea name="name" placeholder="job description" alt="description"  class="{{ $inputStyle }}"></textarea>
+                    <label class="{{ $labelStyle }}">Category</label>
+                    <select name="city" class="{{ $inputStyle }}">
+                        @foreach ($categories as $id => $category)
+                        <option value="{{ $id }}">{{ $category }}</option>
+                        @endforeach
+                    </select>
+                    <label class="{{ $labelStyle }}">Email</label>
+                    <input type="email" name="email" placeholder="your valid email" alt="email" class="{{ $inputStyle }}" />
+                </div>
+                <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Create user</button>
+            </div>        
+        </form>
     </div>
-    <div class="">
-        <label>City</label>
-        <select name="city">
-            @foreach ($cities as $id => $name)
-                <option value="{{$id}}">{{ $name }}</option>
-            @endforeach
-        </select>
-    </div>
-    <div class="">
-        <label>Category</label>
-        <select name="city">
-            @foreach ($categories as $id => $category)
-            <option value="{{ $id }}">{{ $category }}</option>
-            @endforeach
-        </select>
-    </div>
-
-    <div class="">
-        <label>email</label>
-        <input name="email" placeholder="your valid email" alt="email"/>
-    </div>
-    <button type="submit">Create user</button>
-</form>
+</div>
 @stop
